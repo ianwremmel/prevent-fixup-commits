a2f725cd9d19c9119954e4267c62c34080f959bd
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-09-10 12:02:48 -0700

    fix: disable git safe directory feature as it is not relevant in this setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+git config --global --add safe.directory '*'
+
 # for reasons I can't explain, we still need to `git fetch` even with 
 # `fetch-depth: 0`. Perhaps fetch depth only grabs the history of the current 
 # SHA and doesn't bother with branch names?
