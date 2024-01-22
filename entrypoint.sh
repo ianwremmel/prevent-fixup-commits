65d80e9acf2d19e362544a3e0fa878e93638eda0
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2024-01-22 07:45:18 -0800

    feat: use gh to determine default branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 git config --global --add safe.directory '*'
 
+DEFAULT_BRANCH=$(gh repo view --json defaultBranchRef --jq .defaultBranchRef.name)
+
 # for reasons I can't explain, we still need to `git fetch` even with 
 # `fetch-depth: 0`. Perhaps fetch depth only grabs the history of the current 
 # SHA and doesn't bother with branch names?
@@ -12,7 +14,7 @@ git fetch > /dev/null 2> /dev/null
 echo 'Fetched full repository from GitHub.'
 
 echo 'Computing merge base'
-MERGE_BASE=$(git merge-base HEAD origin/master)
+MERGE_BASE=$(git merge-base HEAD "origin/$DEFAULT_BRANCH")
 echo "Found merge base $MERGE_BASE."
 
 if [ "$MERGE_BASE" == "$GITHUB_SHA" ]; then
